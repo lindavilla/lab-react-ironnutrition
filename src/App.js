f8da982c3a92025ff50295f445eb24d43840c198
Linda Villa <linda.villa@alea.com>
2021-06-19 13:09:47 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import Food from "./components/Food";
+import foods from './data/foods.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+state = {
+  searchInput: '',
+  foods: [...foods],
+  selectedFood : [],
+};
+
+handleOnChange = (event) => {
+  this.setState({
+    searchInput: event.target.value,
+  });
+};
+
+handleAddFood = () => {
+  const {selectedFoods} = this.state;
+  const selected = [...selectedFoods];
+
+    const index = selectedFoods.findIndex((ele) => ele.name === food.name )
+    
+    if (index === -1) {
+      selected.push(food);
+      this.setState({
+        selectedFoods : selected,
+      });
+    } else {
+  selected[index].totalCalories = food.totalCalories + selected[index].totalCalories
+  selected[index].qty = food.qty + selected[index].qty
+  this.setState({
+    selectedFoods: selected,
+  });
+};
+
+  selected.push(food);
+  this.setState ({
+    selectedFoods : selected,
+  });
+};
+
+  render () {
+
+    const {foods, searchInput} = this.state;
+    const resultOfFilter = foods.filter((food) => 
+      food.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    const total = selectedFoods.reduce((acc,ele) => ele.totalCalories + acc, 0 )
+
+    return (
+
+      <div className="container mx-auto px-4">
+        <h1>Iron Nutrition</h1>
+        <div className="py-4">
+          <input 
+          type="text" 
+          value = {searchInput}
+          onChange={this.handleOnChange}
+          className="rounded-md border border-grey-600 py-2 px-3 w-full" 
+          placeholder="search"
+
+          />
+        
+        </div>
+        <div className="flex flex-col space-y-4">
+        {resultOfFilter.map((food,index) => {
+        return (
+         <Food 
+          key = {index}
+          name={food.name} 
+          qty = {food.quantity} 
+          calories = {food.calories} 
+          img = {food.image}
+          onAddCart = {this.handleAddFood}
+
+          />
+        );
+      })}
+      <div className="w-1/2"> 
+      <h2>Today's Food</h2>
+      {selectedFood.map((food) => {
+        return 
+        <div key = {index}> {
+          food.name}
+          {food.qty}
+          {food.totalCalories} cal
+          </div>
+      })}
+      <div>total: {total}</div>
+          </div>
+        </div>
+      </div>
+      
+      
+    );
+  }
 }
 
+
 export default App;
+
---
+++ src/components/Food.jsx
@@ -0,0 +1,71 @@
+import {Component} from 'react';
+
+class Food extends Component {
+    state = {
+        qty: 0,
+    };
+
+    handleClick = () => {
+        const {name, calories} = this.props;
+        const {qty} =this.state;
+
+        if (qty !== 0) {
+            this.props.onAddCart ( {
+            name,
+            totalCalories: qty * calories,
+            qty: qty,
+            });
+
+        }
+    }
+
+
+        this.props.onAddCart ({
+            name,
+            totalCalories: qty * calories,
+            qty,
+        });
+        };
+
+    handleInput = (event) => {
+        this.setState({
+            qty: parseInt(event.target.vale),
+        });
+    };
+
+    render () {
+        const{name, img, calories} = this.props
+        const {qty} = this.state;
+
+        return (
+            <div className = 'border border-black-400 shadow-md flex rounded-md h-16 z-10'>
+            <div className= "w-16">
+            <img  className = "w-12" src= {img} alt=''/>
+            </div>
+            <div className="flex-grow px-4 py-3">
+                <h2>{name}</h2>
+                <h3 className="text-grey-400">{calories} cal</h3>
+            </div>
+
+
+            <input 
+            className="w-10"
+            type="number" 
+            value = {qty} 
+            value={qty}
+            onChange={this.handleInput}
+            min = {0}
+            />
+            <button 
+            className="w-8 bg-blue-700 text-white" 
+            onClick = {this.handleClick}
+            >
+            +
+            </button>
+
+            </div>
+        );
+    }
+}
+
+export default Food;
